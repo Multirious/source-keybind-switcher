b4c7ff79431aab55690f2696899ed7771b34610f
Multirious <multirious@outlook.com>
2022-05-21 00:12:36 +0700

    generator init

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,6 @@
-use std::collections::HashMap;
-use std::path::Path;
-use std::fs::File;
-use std::io::{self, BufReader};
-use serde_json::{Value, Map};
-
-use error::*;
-
 mod error {
     use std::result;
+    use std::io;
 
     use super::*;
     
@@ -18,7 +11,8 @@ mod error {
         SerdeJson(serde_json::Error),
         IO(io::Error),
         JsonImportError,
-        CfgTypeNotExist,
+        CfgTypeNotValid,
+        CommandSetUnder2,
     }
     
     #[derive(Debug)]
@@ -58,93 +52,142 @@ mod error {
     }
 }
 
-type ItemShopItems = HashMap<String, String>;
-type ItemShopCategories = HashMap<String, ItemShopItems>;
+mod command_generator {
+    use super::error::*;
 
-pub enum ProgramUiCfg {
-    ItemShop {
-        command: String,
-        categories: ItemShopCategories,
+    struct KeybindSwitcher {
+        key_next: String,
+        key_previous: String,
+        command_sets: Vec<CommandSet>
+    }
+
+    impl KeybindSwitcher {
+        pub fn new(key_next: String, key_previous: String, key_sets: Vec<CommandSet>) -> Self {
+            Self { key_next, key_previous, command_sets: key_sets }
+        }
+    }
+
+    struct CommandSet {
+        alias_name: String,
+        previous_alias: String,
+        next_alias: String,
+        commands: Vec<String>,
+    }
+
+    impl CommandSet {
+        pub fn new(alias_name: String, previous_alias: String, next_alias: String, commands: Vec<String>) -> Self {
+            Self { alias_name, previous_alias, next_alias, commands }
+        }
     }
 }
 
-impl ProgramUiCfg {
-    pub fn from_serde_value(cfg_string: &str, mut value: Value) -> Result<Self> {
-        match cfg_string {
-            "item_shop" => {
-                let command = match value["command"].take() {
-                    Value::String(s) => s,
-                    _ => return Err(Error::new(ErrorKind::JsonImportError, "`command` have to be a string type".to_string())),
-                };
+mod program {
+    use serde_json::Value;
+    use std::collections::HashMap;
+    use super::error::*;
 
-                // Just convert serde map to hashmap
-                // Looks like shit I know
-                // I already forgot what I have done here
-                let categories = match value["categories"].take() {
-                    Value::Object(o) => {
-                        let mut categories = ItemShopCategories::new();
-                        for (ctg_name, items) in o.into_iter() {
-                            let items_serde = match items {
-                                    Value::Object(o) => o,
-                                    _ => return Err(Error::new(ErrorKind::JsonImportError, "value of `categories` object has to be an object type, called `items`".to_string())),
-                            };
-
-                            let mut items = ItemShopItems::new();
-                            for (display, id) in items_serde.into_iter() {
-                                let id = match id {
-                                    Value::String(s) => s,
-                                    _ => return Err(Error::new(ErrorKind::JsonImportError, "all of `items`'s value have to be a string type".to_string())),
+    type ItemShopItems = HashMap<String, String>;
+    type ItemShopCategories = HashMap<String, ItemShopItems>;
+    
+    #[derive(Debug)]
+    pub enum ProgramUiCfg {
+        ItemShop {
+            command: String,
+            categories: ItemShopCategories,
+        }
+    }
+    
+    impl ProgramUiCfg {
+        pub fn from_serde_value(cfg_string: &str, mut value: Value) -> Result<Self> {
+            match cfg_string {
+                "item_shop" => {
+                    let command = match value["command"].take() {
+                        Value::String(s) => s,
+                        _ => return Err(Error::new(ErrorKind::JsonImportError, "`command` have to be a string type".to_string())),
+                    };
+    
+                    // Just convert serde map to hashmap
+                    // Looks like shit I know
+                    // I already forgot what I have done here
+                    let categories = match value["categories"].take() {
+                        Value::Object(o) => {
+                            let mut categories = ItemShopCategories::new();
+                            for (ctg_name, items) in o.into_iter() {
+                                let items_serde = match items {
+                                        Value::Object(o) => o,
+                                        _ => return Err(Error::new(ErrorKind::JsonImportError, "value of `categories` object has to be an object type, called `items`".to_string())),
                                 };
-                                
-                                items.insert(display, id);
+    
+                                let mut items = ItemShopItems::new();
+                                for (display, id) in items_serde.into_iter() {
+                                    let id = match id {
+                                        Value::String(s) => s,
+                                        _ => return Err(Error::new(ErrorKind::JsonImportError, "all of `items`'s value have to be a string type".to_string())),
+                                    };
+                                    
+                                    items.insert(display, id);
+                                }
+    
+                                categories.insert(ctg_name, items);
                             }
-
-                            categories.insert(ctg_name, items);
-                        }
-
-                        categories
-                    },
-                    _ => return Err(Error::new(ErrorKind::JsonImportError, "`categories` has to be an object type".to_string())),
-                };
-                
-                Ok(ProgramUiCfg::ItemShop { command, categories })
-            },
-
-            s => return Err(Error::new(ErrorKind::CfgTypeNotExist, format!("{} is not a valid cfg type", s)))
+    
+                            categories
+                        },
+                        _ => return Err(Error::new(ErrorKind::JsonImportError, "`categories` has to be an object type".to_string())),
+                    };
+                    
+                    Ok(ProgramUiCfg::ItemShop { command, categories })
+                },
+    
+                s => return Err(Error::new(ErrorKind::CfgTypeNotValid, format!("{} is not a valid cfg type", s))),
+            }
+        }
+    }
+    
+    #[derive(Debug)]
+    pub struct Program {
+        cfg: ProgramUiCfg,
+    }
+    
+    impl Program {
+        pub fn parse_serde_value(mut value: Value) -> Result<Self> {
+            if !value.is_object() {
+                return Err(Error::new(ErrorKind::JsonImportError, "json file has to have object type first".to_string()))
+            }
+            
+    
+            let cfg = match value["cfg"].take() {
+                Value::String(s) => s,
+                _ => return Err(Error::new(ErrorKind::JsonImportError, "".to_string())),
+            };
+    
+            let pcfg = ProgramUiCfg::from_serde_value(&cfg, value);
+    
+            return match pcfg {
+                Ok(o) => Ok(Program { cfg: o }),
+                Err(e) => Err(e),
+            }
+        }
+    
+        pub fn item_shop_generate(&self) -> String {
+            todo!()
         }
     }
 }
 
-pub struct Program {
-    cfg: ProgramUiCfg,
-}
+mod data{
+    use std::path::Path;
+    use std::fs::File;
+    use std::io::BufReader;
+    use serde_json::Value;
+    use super::error::*;
 
-impl Program {
-    pub fn parse_serde_value(mut value: Value) -> Result<Self> {
-        if !value.is_object() {
-            return Err(Error::new(ErrorKind::JsonImportError, "json file has to have object type first".to_string()))
-        }
-        
-
-        let cfg = match value["cfg"].take() {
-            Value::String(s) => s,
-            _ => return Err(Error::new(ErrorKind::JsonImportError, "".to_string())),
-        };
-
-        let pcfg = ProgramUiCfg::from_serde_value(&cfg, value);
-
-        return match pcfg {
-            Ok(o) => Ok(Program { cfg: o }),
-            Err(e) => Err(e),
-        }
+    pub fn load_json<P: AsRef<Path>>(path: P) -> Result<Value> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+    
+        let val = serde_json::from_reader::<BufReader<File>, Value>(reader)?;
+    
+        Ok(val)
     }
-}
-
-pub fn load_json<P: AsRef<Path>>(path: P) -> Result<Value> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    let val = serde_json::from_reader::<BufReader<File>, Value>(reader)?;
-
-    Ok(val)
 }
